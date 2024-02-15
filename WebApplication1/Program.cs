9765afdbdc56e1893923b1abc03299e0b0f48f49
VovaGul <vovagul1999@mail.ru>
2024-02-15 11:55:07 +0500

    вынес строку подключения в настройки

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,15 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using WebApplication1;
 using WebApplication1.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<Context>(contextBuilder => contextBuilder
-    .UseSqlServer(@"server=(localdb)\mssqllocaldb;database=CrudDb;trusted_connection=true;"));
+builder.Services.AddDbContext<Context>((provider, contextBuilder)=> contextBuilder
+    .UseSqlServer(provider.GetRequiredService<IOptions<DatabaseSettings>>().Value.ConnectionString));
 
 var app = builder.Build();
 
@@ -25,72 +27,4 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-using var a = app.Services.CreateScope();
-var context = a.ServiceProvider.GetRequiredService<Context>();
-
-
-var cream = new Good()
-{
-    Name = "Крем",
-    Price = 20,
-    Category = "Косметика",
-    Weight = 10
-};
-
-var paste = new Good()
-{
-    Name = "Паста",
-    Price = 10,
-    Category = "Машины"
-};
-
-var pomade = new Good()
-{
-    Name = "Помада",
-    Price = 3,
-    Category = "Косметика"
-};
-await context.Database.EnsureDeletedAsync();
-await context.Database.EnsureCreatedAsync();
-await context.Goods.AddRangeAsync(new List<Good>()
-{
-    cream,
-    paste,
-    pomade
-});
-
-await context.Sales.AddRangeAsync(new List<Sale>()
-{
-    new Sale()
-    {
-        Good = cream,
-        SalesAmount = 1000,
-        SaleDate = DateTime.Now
-    },
-    new Sale()
-    {
-        Good = cream,
-        SalesAmount = 5,
-        SaleDate = new DateTime(2020, 10, 11)
-    },
-    new Sale()
-    {
-        Good = cream,
-        SalesAmount = 1005,
-        SaleDate = DateTime.Now
-    },
-    new Sale()
-    {
-        Good = paste,
-        SalesAmount = 1002,
-        SaleDate = DateTime.Now
-    },
-    new Sale()
-    {
-        Good = pomade,
-        SalesAmount = 10,
-        SaleDate = DateTime.Now
-    }
-});
-await context.SaveChangesAsync();
 app.Run();
