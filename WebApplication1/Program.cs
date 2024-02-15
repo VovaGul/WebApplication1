5ad7af97bed59c94f1a5b99caf6aba047fc6ca74
VovaGul <vovagul1999@mail.ru>
2024-02-15 18:36:53 +0500

    добавил иницилазацию тестовую

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -31,6 +31,83 @@ using (var scope = app.Services.CreateScope())
     var context = scope.ServiceProvider.GetRequiredService<ShopContext>();
     await context.Database.EnsureDeletedAsync();
     await context.Database.EnsureCreatedAsync();
+    var cream = new Good()
+    {
+        Name = "крем",
+        Price = 20,
+        Category = "косметика",
+        Weight = 10
+    };
+
+    var paste = new Good()
+    {
+        Name = "паста",
+        Price = 10,
+        Category = "машины",
+        Weight = 15
+    };
+
+    var pomade = new Good()
+    {
+        Name = "помада",
+        Price = 3,
+        Category = "косметика",
+        Weight = 5
+    };
+    await context.Database.EnsureCreatedAsync();
+    await context.Goods.AddRangeAsync(new List<Good>()
+    {
+        cream,
+        paste,
+        pomade
+    });
+
+    await context.Sales.AddRangeAsync(new List<Sale>()
+    {
+        new Sale()
+        {
+            Good = cream,
+            SalesAmount = 3000,
+            SaleDate = new DateTime(2020, 10, 10)
+        },
+        new Sale()
+        {
+            Good = cream,
+            SalesAmount = 500,
+            SaleDate = new DateTime(2020, 10, 10)
+        },
+        new Sale()
+        {
+            Good = cream,
+            SalesAmount = 50,
+            SaleDate = new DateTime(2020, 10, 11)
+        },
+        new Sale()
+        {
+            Good = cream,
+            SalesAmount = 2000,
+            SaleDate = DateTime.Now
+        },
+        new Sale()
+        {
+            Good = paste,
+            SalesAmount = 2000,
+            SaleDate = new DateTime(2020, 10, 11)
+        },
+        new Sale()
+        {
+            Good = pomade,
+            SalesAmount = 10,
+            SaleDate = new DateTime(2020, 10, 11)
+        },
+        new Sale()
+        {
+            Good = pomade,
+            SalesAmount = 2000,
+            SaleDate = DateTime.Now
+        }
+    });
+    await context.SaveChangesAsync();
 }
 
 app.Run();
